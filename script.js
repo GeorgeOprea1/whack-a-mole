397e6428551dbf1f56b57dea4f2a71c48ba74ed4
GeorgeOprea <oprea.george14@yahoo.com>
2023-11-09 19:09:00 +0200

    js functionality added

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
 const holes = document.querySelectorAll(".hole");
 const score = document.querySelector(".score");
 const moles = document.querySelectorAll(".moles");
+const startBtn = document.querySelector("#start");
+
+let lastHole;
+let timeUp = false;
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHoles(holes) {
+  const index = Math.floor(Math.random() * holes.length);
+  const hole = holes[index];
+
+  if (hole === lastHole) {
+    console.log("same hole");
+    return randomHoles(holes);
+  }
+
+  lastHole = hole;
+  return hole;
+}
+
+function molePop() {
+  const time = randomTime(200, 1000);
+  const hole = randomHoles(holes);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) molePop();
+  }, time);
+}
+
+function startGame() {
+  score.textContent = 0;
+  timeUp = false;
+  molePop();
+  setTimeout(() => (timeUp = true), 2000);
+}
+
+startBtn.addEventListener("click", startGame);
